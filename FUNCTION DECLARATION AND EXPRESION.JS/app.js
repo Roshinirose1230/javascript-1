b44f36d8a05cee3cc56e58389ac9e70a3a0037e6
Roshinirose1230 <aroshinibee@gmail.com>
2021-06-07 17:06:32 +0530

    first commit

---
+++ FUNCTION DECLARATION AND EXPRESION.JS/app.js
@@ -0,0 +1,65 @@
+// FUNCTION DECLARATIONS
+//basics
+//Function greet() {
+ // console.log('hello');
+//}
+//greet();//function call
+
+//or
+//function greet(){
+  //return('hello');
+//}
+//console.log(greet());
+
+function greet(firstName = 'John', lastName = 'Doe'){
+  // if(typeof firstName === 'undefined'){firstName = 'John'}
+  // if(typeof lastName === 'undefined'){lastName = 'Doe'}
+  //console.log('Hello');
+  return 'Hello ' + firstName + ' ' + lastName;
+}
+
+// console.log(greet());
+
+// FUNCTION EXPRESIONS
+
+const square = function(x = 3){
+  return x*x;
+};
+ console.log(square());
+
+//OR
+//const square = function(x =){
+  //return x*x;
+//};
+
+
+ //console.log(square(8));
+
+// IMMIDIATLEY INVOKABLE FUNCTION EXPRESSIONS - IIFEs
+
+ (function(){
+   console.log('IIFE Ran..');
+ })();
+
+(function(name){
+   console.log('Hello '+ name);
+ })('Brad');
+
+// PROPERTY METHODS
+
+const todo = {
+  add: function(){
+    console.log('Add todo..');
+  },
+  edit: function(id){
+    console.log(`Edit todo ${id}`);
+  }
+}
+
+todo.delete = function(){
+  console.log('Delete todo...');
+}
+
+todo.add();
+todo.edit(22);
+todo.delete();
